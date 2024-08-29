4a2dc97b3c5a553be0b70c89134ce81eeaac9df7
nadiarusdiana <144987472+nadiarusdiana@users.noreply.github.com>
2024-08-29 09:32:45 +0700

    Add files via upload

---
+++ Bab2Soal1/src/main/java/com/mycompany/bab2soal1/Bab2Soal1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bab2soal1;
+
+/**
+ *
+ * @author Nadia Rusdiana
+ */
+public class Bab2Soal1 {
+
+    public static void main(String[] args) {
+        int a=50;
+        float b=a;
+        
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
